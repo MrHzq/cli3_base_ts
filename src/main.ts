7fad24790aa579d0fca9201e442fad2f59ccdec4
hzq <13402318457@163.com>
2019-07-31 18:03:14 +0800

    完成一部分

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueTool from 'vue-tool-plugin'
 
-Vue.config.productionTip = false;
+Vue.use(VueTool, { router, prefix: 'lalal' })
+
+console.log(Vue.prototype)
+
+Vue.prototype.$mm = '妹妹'
+
+// 全局方法 this. 的调用方式
+declare module 'vue/types/vue' {
+    interface Vue {
+        $mm: any
+        $setItem: any
+        $tool: any
+        $to: any
+    }
+}
+
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import { Component, Vue } from 'vue-property-decorator'
+    import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    @Component({
+        components: {
+            HelloWorld
+        }
+    })
+    export default class Home extends Vue {
+        created() {
+            console.log(this.$mm)
+            console.log(this.$tool.fprice())
+            console.log(this.$tool.fdate())
+            this.$tool.setItem()
+
+            setTimeout(() => {
+                this.$tool.to('/about')
+            }, 5000)
+        }
+    }
 </script>
